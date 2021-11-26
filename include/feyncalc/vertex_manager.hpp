d25fd431488db5ea8323bb070d68f89ab9d4145d
nittdeniz <51919161+nittdeniz@users.noreply.github.com>
2021-11-26 16:05:09 +0200

    added vertex_manager

--- include/feyncalc/vertex_manager.hpp
+++ include/feyncalc/vertex_manager.hpp
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <map>
+#include <utility>
 #include <string>
 
 #include "matrix.hpp"
@@ -12,13 +13,28 @@ namespace Feyncalc
 {
     class Vertex_Manager
     {
-        using Vertex_Function = std::function<Matrix()>;
-    private:
-        std::map<std::string, Vertex_Function> _vertex_rules;
     public:
-        void add_vertex(std::vector<Particle_Ptr>&& particles, Vertex_Function&& function);
+        enum class Direction : unsigned int
+        {
+            IN = 0x01,
+            OUT = 0x02,
+            BOTH = 0x03
+        };
+        using Vertex_Function = std::function<Matrix()>;
+        using String_Direction_Pair = std::pair<std::string, Direction>;
+    private:
+        std::map<
+                std::string,
+                std::map<std::vector<Direction>, Vertex_Function>
+                > _vertex_rules;
 
-        Vertex_Function get_vertex(std::vector<Particle_Ptr> const& particles);
+        std::pair<std::string, std::vector<Direction>> generate_keys(std::vector<String_Direction_Pair> pairs) const;
+    public:
+
+
+        void add_vertex( Vertex_Function const& function, std::vector<std::pair<Particle_Ptr, Direction>> const& particles);
+
+        Vertex_Function get_vertex(std::vector<String_Direction_Pair> const& pairs) const;
     };
 }
 #endif // FEYNCALC_VERTEX_MANAGER_HPP
--- source/vertex_manager.cpp
+++ source/vertex_manager.cpp
@@ -1,1 +1,76 @@
 #include "vertex_manager.hpp"
+#include <iostream>
+
+namespace Feyncalc
+{
+    void Vertex_Manager::add_vertex( Vertex_Manager::Vertex_Function const& function, std::vector<std::pair<Particle_Ptr, Vertex_Manager::Direction>> const& particles)
+    {
+        using std::abort;
+        using std::cerr;
+        using std::string;
+        using std::vector;
+
+        vector<String_Direction_Pair> pairs;
+        pairs.reserve(particles.size());
+        for( auto const& pair : particles )
+        {
+            pairs.emplace_back(pair.first->name(), pair.second);
+        }
+
+        const auto keys = generate_keys(pairs);
+        _vertex_rules[keys.first][keys.second] = function;
+    }
+
+    Vertex_Manager::Vertex_Function Vertex_Manager::get_vertex(std::vector<String_Direction_Pair> const& pairs) const
+    {
+        const auto keys = generate_keys(pairs);
+        try
+        {
+            auto particle_matches = _vertex_rules.at(keys.first);
+            for( auto const& match : particle_matches )
+            {
+                if( match.first.size() != keys.second.size() )
+                {
+                    continue;
+                }
+                bool found = true;
+                for( std::size_t i = 0; i < keys.second.size(); ++i )
+                {
+                    found &= static_cast<unsigned int>(match.first[i]) &
+                             static_cast<unsigned int>(keys.second[i]);
+                }
+                if( found )
+                {
+                    return match.second;
+                }
+            }
+        }
+        catch( std::out_of_range const& exception )
+        {
+            std::cerr << "Did not find vertex: " << keys.first << "\n";
+            abort();
+        }
+        std::cerr << "Vertex does not support particle configuration.\n";
+        abort();
+    }
+
+    std::pair<std::string, std::vector<Vertex_Manager::Direction>> Vertex_Manager::generate_keys(std::vector<String_Direction_Pair> pairs) const
+    {
+        std::sort(pairs.begin(), pairs.end(), [](String_Direction_Pair const& lhs, String_Direction_Pair const& rhs){
+            return lhs.first < rhs.first;
+        });
+
+        string ID;
+        vector<Direction> directions;
+        directions.reserve(pairs.size());
+        for( auto const& pair : pairs )
+        {
+            ID += pair.first;
+            directions.push_back(pair.second);
+        }
+        return {ID, directions};
+    }
+
+}
+
+
